2e46f09688f7e13944c2c843b5b64add7a0bd526
Sam Goldberg <sgoldber61@gmail.com>
2018-08-22 14:08:33 -0700

    create/save doc db controller

--- index.js
+++ index.js
@@ -30,6 +30,7 @@ require('./services/googlePassport')(pool);
 
 // create server routes
 require('./routes/auth')(app);
+require('./routes/documents')(app, pool);
 
 // set up server
 const port = 4000;
