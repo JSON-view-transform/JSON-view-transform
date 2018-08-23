66eecb873d43ef2e98dbbb1103c86fe215306101
Sam Goldberg <sgoldber61@gmail.com>
2018-08-22 21:59:00 -0700

    fixed small bug in primitive.js

--- client/src/components/Primitive.jsx
+++ client/src/components/Primitive.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 const Primitive = (props) => {
 
   return <div className="primitive" style= {{ textIndent: `${props.indent}px` }}>
-      {props.key} : "{props.data}"
+      {props.name} : "{props.data}"
     </div>
 }
 export default Primitive;
